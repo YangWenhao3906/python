2548fd5ae294a4a890f0353b867ebf1b4dce1002
Wenhao Yang <72293459+YangWenhao3906@users.noreply.github.com>
2023-07-05 15:03:43 +0000

    0004 1. 改成文件输出, 2. 发现split()的bug: 无法去除所有标点, 比如测试用例中, 同时存在"together","together,","together."

--- yangwenhao3906/0004/solve.py
+++ yangwenhao3906/0004/solve.py
@@ -1,14 +1,20 @@
+# 任一个英文的纯文本文件，统计其中的单词出现的个数。
+
 from collections import Counter
 
 def word_frequency_count(file_name):
-    with open("text.txt","r") as file:
+    with open(file_name,"r") as file:
         content = file.read()
 
+    # TODO split()没有去除所有标点
+    # 比如测试用例中, 同时存在"together","together,","together."
     words = content.split()
 
     word_counts = Counter(words).most_common()
 
-    for word, count in word_counts:
-        print(f"{word}: {count}")
+    with open("word_frequency_"+file_name,"w") as dst_file:
+        for word, count in word_counts:
+            print(f"{word}: {count}")
+            dst_file.write(f"{word}: {count}\n")
 
 word_frequency_count("text.txt")
